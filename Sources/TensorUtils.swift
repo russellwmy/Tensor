dc423c22c8f5c8f906315e5c6e3f1cc7d09fe7b9
Russell Wong <russellwmy@gmail.com>
2016-07-10 01:08:09 +0800

    added random Tensor creation

--- Sources/TensorUtils.swift
+++ Sources/TensorUtils.swift
@@ -1,26 +1,6 @@
-extension Int {
-    internal func ceilDiv(right: Int) -> Int {
-        return (self + right - 1) / right
+import Foundation
+extension Tensor {
+    public var random: Tensor {
+        return Tensor(shape: shape, elements: elements.map { drand48() })
     }
-}
-
-internal func hasSuffix<Element: Equatable>(array: [Element], suffix: [Element]) -> Bool {
-    guard array.count >= suffix.count else { return false }
-    return zip(array[(array.count - suffix.count)..<array.count], suffix).reduce(true) { $0 && $1.0 == $1.1 }
-}
-
-internal func zipMap(_ a: [Float], _ b: [Float], operation: (Float, Float) -> Float) -> [Float] {
-    var result: [Float] = []
-    for i in a.indices {
-        result.append(operation(a[i], b[i]))
-    }
-    return result
-}
-
-internal func zipMapRepeat(_ a: [Float], _ infiniteB: [Float], operation: (Float, Float) -> Float) -> [Float] {
-    var result: [Float] = []
-    for i in a.indices {
-        result.append(operation(a[i], infiniteB[i % infiniteB.count]))
-    }
-    return result
 }
